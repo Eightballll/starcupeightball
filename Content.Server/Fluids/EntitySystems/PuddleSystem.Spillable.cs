e078a6a4064bb749a697078088fccb1a97347bdb
0vrseer <iov3rseeri@gmail.com>
2025-08-03 04:35:20 -0400

    Beer googles function as their description states. (#4091)
    * SafeSolutionThrower Component
    
    * Edge cases + poor attempt at angle fixing
    
    * PhysicsSystem to fix rotation
    
    * newline accidental
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * I hope the dv init relay isn't slop
    
    * fix spacing
    
    * RefRelayInventoryEvent is a protected method
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- Content.Server/Fluids/EntitySystems/PuddleSystem.Spillable.cs
+++ Content.Server/Fluids/EntitySystems/PuddleSystem.Spillable.cs
@@ -3,11 +3,18 @@ using Content.Shared.Database;
 using Content.Shared.Fluids.Components;
 using Content.Shared.Spillable;
 using Content.Shared.Throwing;
+using Content.Shared.Weapons.Melee.Events;
+using Robust.Shared.Player;
+using Content.Shared._DV.Chemistry.Systems; // DeltaV Beergoggles enable safe throw
+using Robust.Shared.Physics.Systems; // DeltaV Beergoggles enable safe throw
 
 namespace Content.Server.Fluids.EntitySystems;
 
 public sealed partial class PuddleSystem
 {
+    [Dependency] private readonly SharedPhysicsSystem _physics = default!; // DeltaV - Beergoggles enable safe throw
+    [Dependency] private readonly SafeSolutionThrowerSystem _safesolthrower = default!; // DeltaV - Beergoggles enable safe throw
+
     protected override void InitializeSpillable()
     {
         base.InitializeSpillable();
@@ -40,12 +47,40 @@ public sealed partial class PuddleSystem
 
         if (args.User != null)
         {
-            AdminLogger.Add(LogType.Landed,
+            // DeltaV - start of Beergoggles enable safe throw
+            if (_safesolthrower.GetSafeThrow(args.User.Value))
+            {
+                _physics.SetAngularVelocity(entity, 0);
+                Transform(entity).LocalRotation = Angle.Zero;
+                return;
+            }
+            // DeltaV - end of Beergoggles enable safe throw
+            _adminLogger.Add(LogType.Landed,
                 $"{ToPrettyString(entity.Owner):entity} spilled a solution {SharedSolutionContainerSystem.ToPrettyString(solution):solution} on landing");
         }
 
         var drainedSolution = _solutionContainerSystem.Drain(entity.Owner, soln.Value, solution.Volume);
         TrySplashSpillAt(entity.Owner, Transform(entity).Coordinates, drainedSolution, out _);
+    }
+
+    /// <summary>
+    /// Prevent Pacified entities from throwing items that can spill liquids.
+    /// </summary>
+    private void OnAttemptPacifiedThrow(Entity<SpillableComponent> ent, ref AttemptPacifiedThrowEvent args)
+    {
+        // Don’t care about closed containers.
+        if (Openable.IsClosed(ent))
+            return;
+
+        // Don’t care about empty containers.
+        if (!_solutionContainerSystem.TryGetSolution(ent.Owner, ent.Comp.SolutionName, out _, out var solution) || solution.Volume <= 0)
+            return;
+
+        // DeltaV - start of Beergoggles enable safe throw
+        if (_safesolthrower.GetSafeThrow(args.PlayerUid))
+            return;
+        // DeltaV - end of Beergoggles enable safe throw
+        args.Cancel("pacified-cannot-throw-spill");
     }
 
     private void OnDoAfter(Entity<SpillableComponent> entity, ref SpillDoAfterEvent args)
